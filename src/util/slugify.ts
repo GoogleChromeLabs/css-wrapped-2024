60e57e934a97a04e2b3d559ccba2efc67423d09c
Bramus <bramus@bram.us>
2024-09-30 17:37:54 +0200

    Dynamically build the aside

---
+++ src/util/slugify.ts
@@ -0,0 +1,24 @@
+export default function (title: string) {
+    if (!title) {
+        return "NOT SET";
+    }
+    return (
+        title
+        // remove leading & trailing whitespace
+        .trim()
+        // remove code tags
+        .replaceAll('<code>', '')
+        .replaceAll('</code>', '')
+        // remove code tags
+        .replaceAll('&lt;', '')
+        .replaceAll('&gt;', '')
+        // remove special characters
+        .replace(/[^A-Za-z0-9 ]/g, '')
+        // replace spaces
+        .replace(/\s+/g, '-')
+        // remove leading & trailing separtors
+        .replace(/^-+|-+$/g, '')
+        // output lowercase
+        .toLowerCase()
+    )
+}
