d5cddbd4de89f7b285034ab98525134fd4cbe092
Bramus <bramus@bram.us>
2024-10-08 21:56:35 +0200

    Add missing LICENSE header

--- src/util/shuffle.ts
+++ src/util/shuffle.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export default function (arr:string[]) {
   for (let i = arr.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
