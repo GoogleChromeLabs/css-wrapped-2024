966e6e423b095d0674c5bfae7e90b27e53efd466
Adam Argyle <argyle@google.com>
2024-11-14 16:31:14 -0800

    hide devtoolbar, effecting perf inspection

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   integrations: [icon()],
 });
